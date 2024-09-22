4c0e6469f3bafc2b40f38cef3ff6384383c1bff4
HShubham3 <hadoleshubh22@gmail.com>
2024-09-22 15:47:46 +0530

    first commit

---
+++ src/test/java/com/project/uber/uberapp/UberappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uber.uberapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
